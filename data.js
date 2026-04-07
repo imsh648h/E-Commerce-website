7ef9597cb260769e7b39ff8fd1fa640b3a11be24
MI Nethmini <imashamanawaduge572@gmail.com>
2026-04-07 14:33:58 +0530

    Add product data with images and pricing

---
+++ data.js
@@ -0,0 +1,42 @@
+import p1_img from "./b1.png";
+import p2_img from "./b2.png";
+import p3_img from"./b3.png";
+import p4_img from "./b4.png";
+
+
+const data_product = [
+    {
+        id:1,
+        name:"Serica Cardigan",
+        image:p1_img,
+        new_price: 1500.00,
+        old_price: 2000.00,
+
+    },
+   {
+        id:2,
+        name:"Mira Kit",
+        image:p2_img,
+        new_price: 2500.00,
+        old_price: 2800.00,
+
+    }, 
+    {
+        id:3,
+        name:"Woven Handbag",
+        image:p3_img,
+        new_price: 1800.00,
+        old_price: 2000.00,
+
+    },
+    {
+        id:4,
+        name:"Pixel Check Blouse",
+        image:p4_img,
+        new_price: 2800.00,
+        old_price: 3600.00,
+
+    }
+]
+
+export default data_product;
