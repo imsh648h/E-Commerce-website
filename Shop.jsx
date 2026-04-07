72f41f06cf5c4a5d48c9b044cae7fbcdc4ca78e4
MI Nethmini <imashamanawaduge572@gmail.com>
2026-04-07 14:30:17 +0530

    (Shop.jsx) component/Pages/Shop.jsx

---
+++ Shop.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/Hero/Hero'
+import Popular from '../components/Popular/Popular'
+import Offers from '../components/Offers/Offers'
+
+const shop = () => {
+  return (
+    <div>
+      <Hero></Hero>
+      <Popular/>
+      <Offers/>
+    </div>
+  )
+}
+
+export default shop
