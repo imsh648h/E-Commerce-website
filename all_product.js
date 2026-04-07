c9e5265594f92812c3ca2b73a4c018a7f3be7d9b
MI Nethmini <imashamanawaduge572@gmail.com>
2026-04-07 14:22:21 +0530

    src-assest(all_product.js)
    Added an array of products with details including id, name, category, image, new price, and old price.

---
+++ all_product.js
@@ -0,0 +1,124 @@
+import p1_img from "./b1.png";
+import p2_img from "./b2.png";
+import p3_img from"./b3.png";
+import p4_img from "./b4.png";
+import p5_img from "./ks1.png";
+import p6_img from "./ks2.png";
+import p7_img from "./ks3.png";
+import p8_img from "./ks4.png";
+import p9_img from "./ks5.png";
+import p10_img from "./ks6.png";
+import p11_img from "./ks7.png";
+import p12_img from "./ks8.png";
+
+let all_product=[
+    {
+        id:1,
+        name:"Serica Cardigan",
+        category:"women",
+        image:p1_img,
+        new_price: 1500.00,
+        old_price: 2000.00,
+
+    },
+   {
+        id:2,
+        name:"Mira Kit",
+        category:"women",
+        image:p2_img,
+        new_price: 2500.00,
+        old_price: 2800.00,
+
+    }, 
+    {
+        id:3,
+        name:"Woven Handbag",
+        category:"women",
+        image:p3_img,
+        new_price: 1800.00,
+        old_price: 2000.00,
+
+    },
+    {
+        id:4,
+        name:"Pixel Check Blouse",
+        category:"women",
+        image:p4_img,
+        new_price: 2800.00,
+        old_price: 3600.00,
+
+    },
+    {
+        id:5,
+        name:"boys orange colourblocked hooded sweatshirt",
+        category:"kid",
+        image:p5_img,
+        new_price: 1200.00,
+        old_price: 2000.00,
+
+    },
+  {
+        id:6,
+        name:"boys orange colourblocked hooded sweatshirt",
+        category:"kid",
+        image:p6_img,
+        new_price: 1500.00,
+        old_price: 2000.00,
+
+    },
+      {
+        id:7,
+        name:"boys orange colourblocked hooded sweatshirt",
+        category:"kid",
+        image:p7_img,
+        new_price: 2200.00,
+        old_price: 3000.00,
+
+    },
+      {
+        id:8,
+        name:"boys orange colourblocked hooded sweatshirt",
+        category:"kid",
+        image:p8_img,
+        new_price: 1700.00,
+        old_price: 2000.00,
+
+    },
+      {
+        id:9,
+        name:"boys orange colourblocked hooded sweatshirt",
+        category:"kid",
+        image:p9_img,
+        new_price: 1350.00,
+        old_price: 2000.00,
+
+    },
+       
+      {
+        id:10,
+        name:"boys orange colourblocked hooded sweatshirt",
+        category:"kid",
+        image:p10_img,
+        new_price: 1600.00,
+        old_price: 2000.00,
+
+    },
+      {
+        id:11,
+        name:"boys orange colourblocked hooded sweatshirt",
+        category:"kid",
+        image:p11_img,
+        new_price: 1200.00,
+        old_price: 2000.00,
+
+    },
+      {
+        id:12,
+        name:"boys orange colourblocked hooded sweatshirt",
+        category:"kid",
+        image:p12_img,
+        new_price: 1900.00,
+        old_price: 3200.00,
+
+    },
+];
