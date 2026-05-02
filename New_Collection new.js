8ebb8ac0895c1a5946cf6c43471e722d68cc9a57
MI Nethmini <imashamanawaduge572@gmail.com>
2026-05-02 07:16:45 +0530

    update file(NewCollection.js

---
+++ New_Collection new.js
@@ -0,0 +1,68 @@
+import p13_img from "./nc1.png";
+import p14_img from "./nc2.png";
+import p15_img from"./nc3.png";
+import p16_img from "./nc4.png";
+import p17_img from "./nc5.png";
+import p18_img from "./nc6.png";
+
+const New_Collection = [
+ {
+        id:13,
+        name:"Drape Bow Skirt",
+        category:"women",
+        image:p13_img,
+        new_price: 1890.00,
+        old_price: 2000.00,
+
+    },
+
+    {
+        id:14,
+        name:"PENDORA BADGE THE SCENT 100ML",
+        category:"men",
+        image:p14_img,
+        new_price: 4500.00,
+        old_price: 5590.00,
+
+    },
+
+    {
+        id:15,
+        name:"Peter Pan Crop Top",
+        category:"women",
+        image:p15_img,
+        new_price: 2990.00,
+        old_price: 3500.00,
+
+    },
+
+    {
+        id:16,
+        name:"Floral Print A-Line Midi Dress",
+        category:"women",
+        image:p16_img,
+        new_price: 3800.00,
+        old_price: 4000.00,
+
+    },
+
+    {
+        id:17,
+        name:"Tom Carter - Two-Tone Navy Blue Polo Tee",
+        category:"men",
+        image:p17_img,
+        new_price: 1800.00,
+        old_price: 2000.00,
+
+    },
+
+    {
+        id:18,
+        name:"KIDS FULL KIT",
+        category:"men",
+        image:p18_img,
+        new_price: 1600.00,
+        old_price: 2100.00,
+
+    }
+]
